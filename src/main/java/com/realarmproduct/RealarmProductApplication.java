922973809ce40a11b922c668c6c8c1711967fda5
NOJaeHyuk <jaehyuk7722@naver.com>
2024-12-16 10:15:00 +0900

    first project commit

---
+++ src/main/java/com/realarmproduct/RealarmProductApplication.java
@@ -0,0 +1,13 @@
+package com.realarmproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealarmProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealarmProductApplication.class, args);
+    }
+
+}
